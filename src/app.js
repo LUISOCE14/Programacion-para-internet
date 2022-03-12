407fdd5f1ee2637c010c18e1f02734fd47bfdeb4
LUISOCE14 <92900508+LUISOCE14@users.noreply.github.com>
2022-03-12 13:32:15 -0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import TicTacToe from "./TicTac";
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
